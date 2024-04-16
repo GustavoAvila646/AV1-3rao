05a668ee46f71573174f7e34da96682531d9d34e
Gustavo Avila <105921907+GustavoAvila646@users.noreply.github.com>
2024-04-16 10:23:50 -0300

    .

--- main.js
+++ main.js
@@ -11,5 +11,8 @@ for (let i = 0; i<botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
-    const contadores = document.querySelectorAll(".contador");
-    contadores[0].textContent = "Contagem regressiva";
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-01");
+let tempoAtual = new Date();
+
+contadores[0].textContent = "Contagem regressiva";
