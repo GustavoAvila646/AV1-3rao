2b1388f3669db22866330b61417e847c078e785d
Gustavo Avila <105921907+GustavoAvila646@users.noreply.github.com>
2024-04-30 10:32:04 -0300

    .

--- main.js
+++ main.js
@@ -16,6 +16,16 @@ const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
 const tempoObjetivo2 = new Date("2024-07-05T23:59:59");
 const tempoObjetivo3 = new Date("2024-08-01T23:59:59");
 const tempoObjetivo4 = new Date("2024-12-01T23:59:59");
+
+const tempos = [
+    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+]
+
+for (let i=0; i< contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
     contadores[0].textContent = tempoObjetivo1 - tempoAtual;
