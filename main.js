8a0f00eb7d154a90d3c0eab6aae9054b5bb1545e
Gustavo Avila <105921907+GustavoAvila646@users.noreply.github.com>
2024-04-30 17:47:48 -0300

    .

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ for (let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo2 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo3 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo4 = new Date("2024-12-28T23:59:59");
+const tempoObjetivo1 = new Date("2024-12-31T23:59:59");
+const tempoObjetivo2 = new Date("2024-11-01T23:59:59");
+const tempoObjetivo3 = new Date("2024-08-01T23:59:59");
+const tempoObjetivo4 = new Date("2024-06-01T23:59:59");
 const tempoObjetivo5 = new Date("2025-03-01T23:59:59");
 
 const tempos = [
